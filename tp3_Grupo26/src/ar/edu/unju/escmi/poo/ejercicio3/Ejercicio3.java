73cae293f44a284e2f9361af8d5fe8795e263abb
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-09-12 01:03:48 -0300

    Creacion del menu de opciones en la clase Main- Guari

--- tp3_Grupo26/src/ar/edu/unju/escmi/poo/ejercicio3/Ejercicio3.java
+++ tp3_Grupo26/src/ar/edu/unju/escmi/poo/ejercicio3/Ejercicio3.java
@@ -4,7 +4,23 @@ public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		 int opcion=0;
+		 do {
+		
+			 System.out.println("MENU DE OPCIONES:");
+			 System.out.println("1-Crear empleado");
+			 System.out.println("2-Aumentar Salario");
+			 System.out.println("3-Mostrar la suma total de todos los salarios");
+			 System.out.println("4-Mostrar todos los empleaados");
+			 System.out.println("5-Salir");
+			 
+			 
+		 System.out.print("Ingrese una opcion: ");
+      
+		 }while(opcion != 5);
+		 
+		 
 	}
 
 }
